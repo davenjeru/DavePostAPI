f12a830fdd8d24eecec1e31a355e870db4982eb9
Dave Mathews <davenmathews@gmail.com>
2018-03-03 12:30:33 +0300

    register models to the auth namespace
    this is used by swagger ui for documentation

--- davepostAPI/api/v1/auth/__init__.py
+++ davepostAPI/api/v1/auth/__init__.py
@@ -1,13 +1,15 @@
 from flask_restplus.namespace import Namespace
 
-from davepostAPI.api.v1.auth.login import Login
+from davepostAPI.api.v1.auth.login import Login, user_login_model
 from davepostAPI.api.v1.auth.logout import Logout
-from davepostAPI.api.v1.auth.register import Register
+from davepostAPI.api.v1.auth.register import Register, user_registration_model
 
 auth_ns = Namespace('auth', description='Operations related to authentication')
 
 auth_ns.add_resource(Register, '/register', endpoint='auth_register')
+auth_ns.add_model('user_registration_model', user_registration_model)
 
 auth_ns.add_resource(Login, '/login')
+auth_ns.add_model('user_login_model', user_login_model)
 
 auth_ns.add_resource(Logout, '/logout')
