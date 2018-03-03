1147acaba3d4dc0bd22ee42e3258c9a3c8056109
Dave Mathews <davenmathews@gmail.com>
2018-03-03 12:22:27 +0300

    ft- endpoint view a user's posts
    as well as add tests for the above
    [skip-ci]
    [Finishes|Delivers #155632464 #155632478]

--- davepostAPI/_tests/test_app_users.py
+++ davepostAPI/_tests/test_app_users.py
@@ -3,7 +3,7 @@ from flask_testing import TestCase
 from davepostAPI import app
 from davepostAPI.api.v1 import api_v1
 from davepostAPI.api.v1.auth import Login
-from davepostAPI.api.v1.users import AllUsers, SingleUser
+from davepostAPI.api.v1.users import AllUsers, SingleUser, SingleUserAllPosts
 
 
 class AppTestCase(TestCase):
@@ -29,7 +29,12 @@ class AppTestCase(TestCase):
         self.assertIn(b'"email":', rv.data)
         self.assertIn(b'"url":', rv.data)
 
-    def test_c3_delete_user(self):
+    def test_c3_view_user_posts_empty(self):
+        rv = self.client.get(api_v1.url_for(SingleUserAllPosts, user_id=1))
+        self.assert200(rv)
+        self.assertIn(b'"posts": []', rv.data)
+
+    def test_c4_delete_user(self):
         rv = self.client.delete(api_v1.url_for(SingleUser, user_id=1))
         self.assert401(rv)
         data = dict(email='email@company.com', password='password.Pa55word')
@@ -37,12 +42,12 @@ class AppTestCase(TestCase):
         rv = self.client.delete(api_v1.url_for(SingleUser, user_id=1))
         self.assertEqual(204, rv.status_code)
 
-    def test_c4_view_all_users_empty(self):
+    def test_c5_view_all_users_empty(self):
         rv = self.client.get(api_v1.url_for(AllUsers))
         self.assert200(rv)
         self.assertIn(b'{"users": []', rv.data)
 
-    def test_c5_view_single_user_fail(self):
+    def test_c6_view_single_user_fail(self):
         rv = self.client.get(api_v1.url_for(SingleUser, user_id=1))
         self.assert400(rv)
         self.assertIn(b'User not found!', rv.data)
--- davepostAPI/api/v1/users/single_user_all_posts.py
+++ davepostAPI/api/v1/users/single_user_all_posts.py
@@ -1,6 +1,9 @@
 from flask_restplus import Resource
 from flask_restplus.namespace import Namespace
 
+from davepostAPI.api.v1.boilerplate import check_id_availability, PayloadExtractionError
+from davepostAPI.models import users_list, User
+
 users_ns = Namespace('users')
 
 
@@ -11,7 +14,16 @@ class SingleUserAllPosts(Resource):
         """
         View all posts from a single user
         """
-        pass
+        this_user = None
+        try:
+            this_user = check_id_availability(user_id, users_list, str(User.__name__))
+        except PayloadExtractionError as e:
+            users_ns.abort(e.abort_code, e.msg)
+        my_posts_list = this_user.get_my_posts()
+        my_posts_list_output = []
+        for a_post in my_posts_list:
+            my_posts_list_output.append(a_post.serialize)
+        return dict(posts=my_posts_list_output)
 
     @users_ns.response(201, 'Post created successfully')
     @users_ns.response(400, 'Bad request')
